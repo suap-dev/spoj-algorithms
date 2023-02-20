11603e954ba35d242f0c27808cd782e74626a5e0
suap-dev <suap.dev@gmail.com>
2023-02-20 20:47:05 +0100

    skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
-pub mod pl;
+use std::io::{stdin, Read};
 
 fn main() {
+    let mut input_string: String = String::new();
+
+    stdin().read_line(&mut input_string).expect("Incorrect input");
+    let number_of_tests: u16 = input_string.trim().parse().expect("Incorrect number format");
+
+    for _ in 0..number_of_tests {
+        
+    }
 }
