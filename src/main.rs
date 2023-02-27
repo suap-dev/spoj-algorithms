9b91da8b2adeda14d7536dafafe47432916790c0
suap-dev <suap.dev@gmail.com>
2023-02-27 17:54:33 +0100

    test-stub

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,22 @@ mod pl;
 use std::io::{stdin, BufRead};
 
 fn main() {
+    test();
+}
+
+fn test() {
     let stdin = stdin();
     let mut stdin = stdin.lock();
     let mut buf = String::new();
 
+    // number of tests:
     stdin.read_line(&mut buf).expect("Reading failed");
+    let t: usize = buf.trim().parse().unwrap();
+
+    for _ in 0..t {
+        buf.clear();
+        stdin.read_line(&mut buf).expect("Reading failed");
+        
+        // TODO
+    }
 }
